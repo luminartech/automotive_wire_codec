c4ceb6c584e377f282e8149081f91c33333dd5c1
Zach Heylmun <zheylmun@gmail.com>
2026-07-15 20:46:39 -0400

    feat: add InvalidWidth and InsufficientBuffer error fragments

--- src/lib.rs
+++ src/lib.rs
@@ -13,6 +13,6 @@ mod read;
 mod write;
 pub use decode::{Decode, DecodeIter, DecodeIterator};
 pub use encode::Encode;
-pub use error::{Incomplete, TrailingBytes};
+pub use error::{Incomplete, InsufficientBuffer, InvalidWidth, TrailingBytes};
 pub use read::{read_array, read_be_uint, read_u8, read_u16_be, read_u32_be, read_u64_be, take};
 pub use write::{write_all, write_be_uint, write_u8, write_u16_be, write_u32_be, write_u64_be};
